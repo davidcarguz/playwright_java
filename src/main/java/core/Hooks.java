4a913dd9c36613f9ebbdad3e50b2e58fe94ddc6c
David Cardenas <davidcarguz@gmail.com>
2022-07-24 15:46:39 -0500

    removed unused variables

--- src/main/java/core/Hooks.java
+++ src/main/java/core/Hooks.java
@@ -1,7 +1,6 @@
 package core;
 
 import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 import utils.Utilities;
